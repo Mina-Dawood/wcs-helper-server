4336fea3bff2e31c66e4c7af4c88449e8b87bef3
DawoudMina <minadawoudshahhat@gmail.com>
2020-03-11 07:18:35 +0200

    DEVELOP: Add generic API

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const uat = require('./uat.router');
 const staging2 = require('./staging2.router');
 const prodref = require('./prodref.router');
 const prod = require('./prod.router');
+const request = require('request');
 
 const app = express();
 
@@ -16,6 +17,14 @@ app.use('/api/prod', prod);
 
 app.get('/', (req, res) => {
     res.send('WCS Helper Server is Running ...');
+})
+
+app.get('/api/load-json/:url', (req, res) => {
+    const url = req.params.url;
+    request(url, { json: true }, (err, subRes, body) => {
+        if (err) { return console.log(err); }
+        res.send(subRes.body);
+    });
 })
 
 const port = process.env.PORT || 5000;
